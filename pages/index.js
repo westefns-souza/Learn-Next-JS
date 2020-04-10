217f2fc834e878c4fcf84ef9dd1e80c956bd0a84
westefns-souza <westefns@outlook.com.br>
2020-04-10 13:00:34 -0300

    Getting Started with Next.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../comps/Layout';
+
+export default function Index() {
+    return (
+        <div>
+            <Layout>
+                <p>Hello Next.js</p>
+            </Layout>
+        </div>
+    );
+}
